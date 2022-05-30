5143273d630469a664d29527be8e067f682e4ff4
LusineKhachatryan3113-ios <101631371+LusineKhachatryan3113-ios@users.noreply.github.com>
2022-05-30 14:20:16 +0400

    Update CountryRepository.swift

--- SwiftUISnapChat/Shared/Repositories/CountryRepository.swift
+++ SwiftUISnapChat/Shared/Repositories/CountryRepository.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 import Combine
+
+class CountryRepository: ObservableObject  {
+    @Published var errorMessage: String? = nil
+    @Published var countries: [Country] = []
   
 func fetchCountry(completion: @escaping ([Country]) -> ()) {
        guard  let url = URL(string: "https://gist.githubusercontent.com/anubhavshrimal/75f6183458db8c453306f93521e93d37/raw/f77e7598a8503f1f70528ae1cbf9f66755698a16/CountryCodes.json") else { return }
