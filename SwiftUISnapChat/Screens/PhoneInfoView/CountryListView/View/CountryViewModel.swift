57fdd3b3454ecac59e0cd1806ec89143469cad22
LusineKhachatryan3113-ios <101631371+LusineKhachatryan3113-ios@users.noreply.github.com>
2022-05-31 09:15:40 +0400

    Update CountryViewModel.swift

--- SwiftUISnapChat/Screens/PhoneInfoView/CountryListView/View/CountryViewModel.swift
+++ SwiftUISnapChat/Screens/PhoneInfoView/CountryListView/View/CountryViewModel.swift
@@ -18,29 +18,6 @@ class CountryViewModel: ObservableObject {
     init(countryRepository: CountryRepository) {
         self.countryRepository = countryRepository
     }
-    
-    func errorState() -> CountryViewModel {
-        let fetcher = CountryViewModel(countryRepository: CountryRepository())
-        fetcher.errorMessage = APIError.url(URLError.init(.notConnectedToInternet)).localizedDescription
-        return fetcher
-    }
-    
-    func example1() -> Country {
-        return Country(id: "", code: "AF", dialCode: "+93", name: "Afghanistan")
-        
-    }
-    
-    func example2() -> Country {
-        return Country(id: "", code: "AX", dialCode: "+358", name: "Aland Islands")
-        
-    }
-    
-    func successState() -> CountryViewModel   {
-        let fetcher = CountryViewModel(countryRepository: CountryRepository())
-        fetcher.countries = [example1(), example2()]
-        return fetcher
-    }
-    
     func getflag(country:String) -> String {
         let base : UInt32 = 127397
         var flag = ""
