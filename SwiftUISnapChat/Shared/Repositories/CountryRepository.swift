66c3a6a61f9e8fa91f074ec062722c8ed1cb823f
LusineKhachatryan3113-ios <101631371+LusineKhachatryan3113-ios@users.noreply.github.com>
2022-06-02 10:08:28 +0400

    Update CountryRepository.swift

--- SwiftUISnapChat/Shared/Repositories/CountryRepository.swift
+++ SwiftUISnapChat/Shared/Repositories/CountryRepository.swift
@@ -8,30 +8,28 @@
 import Foundation
 import Combine
 
-class CountryRepository: ObservableObject  {
-    @Published var errorMessage: String? = nil
-    @Published var countries: [Country] = []
+class CountryRepository {
+ 
     let url = URL(string: "https://gist.githubusercontent.com/anubhavshrimal/75f6183458db8c453306f93521e93d37/raw/f77e7598a8503f1f70528ae1cbf9f66755698a16/CountryCodes.json")
     
-    func fetch(url: URL) -> AnyPublisher<Data, APIError> {
+    func fetch<Country: Decodable>(url: URL) -> AnyPublisher<Country, APIError> {
         let request = URLRequest(url: url)
         return URLSession.DataTaskPublisher(request: request, session: .shared)
             .tryMap { data, response in
                 guard let httpResponse = response as? HTTPURLResponse, 200..<300 ~= httpResponse.statusCode else {
                     throw APIError.unknown
                 }
-                return data
+                return data as! Country
             }
             .mapError { error in
-                    if let error = error as? APIError {
-                        return error
-                    } else {
-                        return APIError.badURL
-                    }
+                if let error = error as? APIError {
+                    return error
+                } else {
+                   return APIError.badURL
                 }
+            }
             .eraseToAnyPublisher()
     }
-}
 
 
      
