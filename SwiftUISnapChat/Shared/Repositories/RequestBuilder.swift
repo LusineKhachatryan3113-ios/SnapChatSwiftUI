f1fb34376fc1321b486809343305654ef9bde787
LusineKhachatryan3113-ios <101631371+LusineKhachatryan3113-ios@users.noreply.github.com>
2022-06-07 00:23:41 +0400

    Add files via upload

---
+++ SwiftUISnapChat/Shared/Repositories/RequestBuilder.swift
@@ -0,0 +1,12 @@
+//
+//  RequestBuilder.swift
+//  SwiftUISnapChat
+//
+//  Created by Lusine on 6/5/22.
+//
+
+import Foundation
+
+protocol RequestBuilder {
+    var urlRequest: URLRequest {get}
+}
