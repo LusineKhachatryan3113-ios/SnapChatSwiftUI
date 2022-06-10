d4dfa96eb49786511585fb9cd503120c7d721d45
LusineKhachatryan3113-ios <101631371+LusineKhachatryan3113-ios@users.noreply.github.com>
2022-06-10 12:06:37 +0400

    Update CountryService.swift

--- SwiftUISnapChat/Shared/Repositories/CountryService.swift
+++ SwiftUISnapChat/Shared/Repositories/CountryService.swift
@@ -14,7 +14,7 @@ protocol CountryService {
 }
 
 class RealCountryService: CountryService {
-   @Published var countries: [Country] = []
+    @Published var countries: [Country] = []
     private let session = APISession()
     @Published var savedData: [Components] = []
     let container: NSPersistentContainer
@@ -29,56 +29,18 @@ class RealCountryService: CountryService {
     
     func getCountryList() -> AnyPublisher<[Country], APIError> {
         let model = Countries.init(countries: countries)
-          return session.request(with: CountryEndpoint.countryList)
+        return session.request(with: CountryEndpoint.countryList)
             .handleEvents(receiveOutput: { [self] countries in
-                self.saveData(model: model)
+                saveData(model: model)
             }) .eraseToAnyPublisher()
     }
-
+    
     private func getData() {
         let request = NSFetchRequest<Components>(entityName: "Country")
         do {
             try savedData = container.viewContext.fetch(request)
         } catch {
             print("Error getting data. \(error.localizedDescription)")
-        }
-    }
-    
-    private func getDatafromCoreData() -> [Country] {
-    let moc = DataController.init().container.viewContext
-    let request = NSFetchRequest<Components>(entityName: "Country")
-    let data = try? moc.fetch(request)
-    data.map { $0.forEach { item in
-      item.name! = countries.first?.name ?? "Unknown"
-    }}
-       return countries
-    }
-    
-    private func countryDataVoid() -> AnyPublisher<[Country], APIError> {
-         return Just<Void>(())
-           .setFailureType(to: APIError.self)
-           .map {
-             return getDatafromCoreData()
-           }
-           .eraseToAnyPublisher()
-       }
-    
-    func countryRequest() -> AnyPublisher<[Country], APIError> {
-          let dataFromServer = getCountryList()
-          let dataFromCoreData = countryDataVoid()
-          return Publishers
-            .MergeMany([dataFromServer, dataFromCoreData])
-            .eraseToAnyPublisher()
-        }
-
-    private func replaceModel(model: Countries) {
-        let moc = DataController.init().container.viewContext
-        let request = NSFetchRequest<Components>(entityName: "Country")
-        let data = try? moc.fetch(request)
-        if data?.count == 0 {
-            self.saveData(model: model)
-        } else {
-            self.getData()
         }
     }
     
@@ -89,8 +51,35 @@ class RealCountryService: CountryService {
             city.code = country.code
             city.dialCode = country.dialCode
         }
-        
         try? container.viewContext.save()
     }
+    
+    
+    private func getDatafromCoreData() -> [Country] {
+        let moc = DataController.init().container.viewContext
+        let request = NSFetchRequest<Components>(entityName: "Country")
+        let data = try? moc.fetch(request)
+        data.map { $0.forEach { item in
+            item.name! = countries.first?.name ?? "Unknown"
+        }}
+        return countries
+    }
+    
+    private func countryDataVoid() -> AnyPublisher<[Country], APIError> {
+        Just<Void>(())
+            .setFailureType(to: APIError.self)
+            .map {
+                return getDatafromCoreData()
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    func countryRequest() -> AnyPublisher<[Country], APIError> {
+        let dataFromServer = getCountryList()
+        let dataFromCoreData = countryDataVoid()
+        return Publishers
+            .MergeMany([dataFromServer, dataFromCoreData])
+            .eraseToAnyPublisher()
+    }
+  
 }
-
