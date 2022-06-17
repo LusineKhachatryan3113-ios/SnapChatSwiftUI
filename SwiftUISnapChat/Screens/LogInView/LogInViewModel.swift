d3c7b6790f6807ec857bc5b22ddfd822caa37d01
LusineKhachatryan3113-ios <101631371+LusineKhachatryan3113-ios@users.noreply.github.com>
2022-06-18 01:20:49 +0400

    Update LogInViewModel.swift

--- SwiftUISnapChat/Screens/LogInView/LogInViewModel.swift
+++ SwiftUISnapChat/Screens/LogInView/LogInViewModel.swift
@@ -43,55 +43,6 @@ class LoginViewModel: ObservableObject {
 
 
 
-// @Published var email = ""
-// @Published var password = ""
-    
-//    //MARK: - Validation Functions
-//
-//    func isPasswordValid() -> Bool {
-//        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,15}$")
-//        return passwordTest.evaluate(with: password)
-//    }
-//
-//    func isEmailValid() -> Bool {
-//        let emailTest = NSPredicate(format: "SELF MATCHES %@", "^([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$")
-//        return emailTest.evaluate(with: email)
-//    }
-//
-//    var isSignUpComplete: Bool {
-//        if !isPasswordValid() ||
-//            !isEmailValid() {
-//            return false
-//        }
-//        return true
-//    }
-//
-//    //MARK: - Validation Prompt Strings
-//
-//    var emailPrompt: String {
-//        if isEmailValid(){
-//            return ""
-//        } else {
-//            return "Enter a valid email address"
-//
-//        }
-//    }
-//
-//    var passwordPrompt: String {
-//        if isPasswordValid() {
-//            return ""
-//        } else {
-//            return "Must be between 8 and 15 characters containing at least one number and one capital letter"
-//        }
-//    }
-//    func signup() {
-//         email = ""
-//         password = ""
-//    }
-//}
-
-
-
 
  
  
