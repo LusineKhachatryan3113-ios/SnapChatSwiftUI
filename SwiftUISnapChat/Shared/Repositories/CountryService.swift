0223ac8c62638bbc0e776404ce94c6bdc33e4ae6
LusineKhachatryan3113-ios <101631371+LusineKhachatryan3113-ios@users.noreply.github.com>
2022-06-18 00:54:56 +0400

    Update CountryService.swift

--- SwiftUISnapChat/Shared/Repositories/CountryService.swift
+++ SwiftUISnapChat/Shared/Repositories/CountryService.swift
@@ -6,28 +6,28 @@
 //
 import Foundation
 import Combine
-import CoreData
+import RealmSwift
 import SwiftUI
 
+
 protocol CountryService {
+    
     func getCountryList() -> AnyPublisher<[Country], APIError>
 }
 
 class RealCountryService: CountryService {
-    @Published var countries: [Country] = []
-    private let session = APISession()
-    @Published var savedData: [Components] = []
-    let container: NSPersistentContainer
-    init() {
-        container = NSPersistentContainer(name: "CountryModel")
-        container.loadPersistentStores { (description, error) in
-            if let error = error {
-                fatalError("Error: \(error.localizedDescription)")
-            }
-        }
-        
-    }
     
+    @Published var countries: [Country] = []
+    
+    private let session = APISession()
+    
+    private(set) var localRealm: Realm?
+    
+    @Published var savedData: [CountryList] = []
+    
+    @ObservedResults(CountryList.self) var city
+    
+
     func getCountryList() -> AnyPublisher<[Country], APIError> {
         let dataFromServer = countryRequest()
         let dataFromCoreData = countryDataVoid()
@@ -35,54 +35,57 @@ class RealCountryService: CountryService {
             .MergeMany([dataFromServer, dataFromCoreData])
             .eraseToAnyPublisher()
     }
-    
-    func countryRequest()  -> AnyPublisher<[Country], APIError> {
+
+    func countryRequest() -> AnyPublisher<[Country], APIError> {
         let model = Countries.init(countries: countries)
         return session.request(with: CountryEndpoint.countryList)
             .handleEvents(receiveOutput: { [self] countries in
-                saveData(model: model)
+                saveInRealm(model: model)
             }) .eraseToAnyPublisher()
     }
     
     private func getData() -> AnyPublisher<Countries, APIError> {
-        let request = NSFetchRequest<Components>(entityName: "Country")
-        do {
-            try savedData = container.viewContext.fetch(request)
-        } catch {
-            print("Error getting data. \(error.localizedDescription)")
-        }
+        
         return session.request(with: CountryEndpoint.countryList)
     }
-    
-    private func saveData(model: Countries) {
-        model.countries.forEach { country in
-            let city = Components()
-            city.name = country.name
-            city.code = country.code
-            city.dialCode = country.dialCode
+
+    private func getDatafromRealm() -> [Country] {
+        if let localRealm = localRealm {
+            let allCountries = localRealm.objects(CountryList.self)
+            allCountries.forEach { country in
+                savedData.append(country)
+                country.name = countries.first!.name
+            }
         }
-        
-        try? container.viewContext.save()
-    }
-    
-    private func getDatafromCoreData() -> [Country] {
-        let moc = DataController.init().container.viewContext
-        let request = NSFetchRequest<Components>(entityName: "Country")
-        let data = try? moc.fetch(request)
-        data.map { $0.forEach { item in
-            item.name! = countries.first?.name ?? "Unknown"
-        }}
         return countries
     }
-    
+
     private func countryDataVoid() -> AnyPublisher<[Country], APIError> {
         Just<Void>(())
             .setFailureType(to: APIError.self)
             .map {
-                return getDatafromCoreData()
+                return getDatafromRealm()
             }
             .eraseToAnyPublisher()
+        
         }
-}
+    
+private func saveInRealm(model: Countries) {
+      let realm = try? Realm()
+      model.countries.forEach { country in
+        do {
+          let count = CountryList()
+          try realm?.write {
+            count.name = country.name
+            count.code = country.code
+            count.dialCode = country.dialCode
+            realm?.add(count)
+          }
+        } catch {
+          print(error)
+        }
+      }
+    }
+  }
 
 
