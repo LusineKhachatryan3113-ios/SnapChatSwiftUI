d6f90a8df6eb933f94738b26787cc307c983cd1e
LusineKhachatryan3113-ios <101631371+LusineKhachatryan3113-ios@users.noreply.github.com>
2022-06-18 01:09:12 +0400

    Update AfterRegisterLogInView.swift

--- SwiftUISnapChat/Screens/AfterRegisterView/AfterRegisterLogInView.swift
+++ SwiftUISnapChat/Screens/AfterRegisterView/AfterRegisterLogInView.swift
@@ -9,7 +9,25 @@
 import SwiftUI
 
 struct AfterRegisterLogInView: View {
+    @State var isLoggedIn: Bool = false
+    @EnvironmentObject var authentication: Authentication
     var body: some View {
-Text("This is LogIn After Register View")
+        NavigationView {
+            VStack {
+                Text("Authorized.. You are in")
+                    .font(.largeTitle)
+              }
+            .padding()
+            .navigationTitle("My secure App")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Log out") {
+                        authentication.updateValidation(success: false)
+                        
+                    }
+                }
+            }
+        }
     }
 }
+
